8cc8f72d8be7ecb4fad34a5ebc26c57d352d5973
alvaromoca1 <alvaromoca1@gmail.com>
2021-11-24 19:09:30 -0500

    create function for cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression, Interval } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  //creamos un loger
+  private readonly logger = new Logger(AppService.name);
+
+  //@Cron("10 * * * * *")//10 segundos en todos los minutos, a todas las horas, de todos los dias del mes y de todos los dias de la semana
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  //creamos un metodo
+  showEveryFiSeconds(){
+    this.logger.debug("Este mensaje se muestra cada 10 segundos")
+  }
+
+  //podemos realizar un interval Method
+  @Interval(5000)
+  intervalMethod(){
+    this.logger.debug("Este mensaje se muestra cada 5 segundos")
+
   }
 }
